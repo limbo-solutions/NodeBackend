c2242c9534f9749d5aca5a1f2cdcef1fd9fd99ef
centpays-tech <centpaysaws@gmail.com>
2024-03-16 15:05:00 +0530

    app.js

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const AcquirerRoutes = require("./src/routes/acquirerroute");
 const TransactiontableRoutes = require("./src/routes/transactiontableroute");
 const RatetableRoutes = require("./src/routes/ratetableroute");
 const SettlementtableRoutes = require("./src/routes/settlementroute");
+const BusinesstypeRoutes = require("./src/routes/businesstyperoute");
+const CategoryRoutes = require("./src/routes/categoryroute");
+const CreatecurrencyRoutes = require("./src/routes/createcurrencyroute");
+const DocumenttypeRoutes = require("./src/routes/documenttyperoute");
+const CreatebankRoutes = require("./src/routes/createbankroute");
+const BusinesssubcategoryRoutes = require("./src/routes/businesssubcategoryroute");
+const DocumentcategoryRoutes = require("./src/routes/documentcategoryroute");
 
 require("./src/config/database");
 
@@ -17,9 +24,6 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
-// app.use("", (req, res) => {
-//   res.send("Welcome to Centpays");
-// });
 app.use("/", ClientRoutes);
 app.use("/", UserRoutes);
 app.use("/", SessionRoutes);
@@ -27,6 +31,13 @@ app.use("/", AcquirerRoutes);
 app.use("/", TransactiontableRoutes);
 app.use("/", RatetableRoutes);
 app.use("/", SettlementtableRoutes);
+app.use("/", BusinesstypeRoutes);
+app.use("/", CategoryRoutes);
+app.use("/", CreatecurrencyRoutes);
+app.use("/", DocumenttypeRoutes);
+app.use("/", CreatebankRoutes);
+app.use("/", BusinesssubcategoryRoutes);
+app.use("/", DocumentcategoryRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
