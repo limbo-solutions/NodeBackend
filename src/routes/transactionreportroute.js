4117b7a2ae11505ca45da389577c26f121e3943f
centpays-tech <centpaysaws@gmail.com>
2024-05-29 13:45:54 +0530

    transaction search added

--- src/routes/transactionreportroute.js
+++ src/routes/transactionreportroute.js
@@ -2,14 +2,7 @@ const express = require("express");
 const { verifyToken } = require("../middlewares/verifyToken");
 const {
   searchTransactionReport,
-  quickSearch,
-  multiTransactions,
-  transactionsToday,
-  transactionsYesterday,
-  transactionsLast7Days,
-  transactionsLast15Days,
-  transactionsLast30Days,
-  transactionsLastYear,
+  quickSearch
 } = require("../controllers/reportscontroller");
 
 const router = express.Router();
@@ -17,12 +10,5 @@ const router = express.Router();
 // Define user routes
 router.post("/transactionreport", verifyToken, searchTransactionReport);
 router.get("/transactionreport", verifyToken, quickSearch);
-router.post("/multisearch", verifyToken, multiTransactions);
-router.get("/reporttoday", verifyToken, transactionsToday);
-router.get("/reportyesterday", verifyToken, transactionsYesterday);
-router.get("/reportlast7days", verifyToken, transactionsLast7Days);
-router.get("/reportlast15days", verifyToken, transactionsLast15Days);
-router.get("/reportlast30days", verifyToken, transactionsLast30Days);
-router.get("/reportlastyear", verifyToken, transactionsLastYear);
 
 module.exports = router;
