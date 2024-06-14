d43d046a36012c6350ef50317da35059eb4cea2d
centpays-tech <centpaysaws@gmail.com>
2024-06-14 12:11:17 +0530

    server update checking

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome! The server is running fine");
+  res.send("Welcome! The server is running and updating fine");
 });
 
 app.use(bodyParser.json());
